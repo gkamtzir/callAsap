9b9ac7e92401e1249a999edaeece04333186003d
George Kamtziridis <georgekam96@gmail.com>
2016-09-20 21:04:49 +0300

    First method

--- api.dist.php
+++ api.dist.php
@@ -1,5 +1,6 @@
 <?php
 
+  require "vendor/autoload.php";
 
   function getDB() {
 
@@ -15,3 +16,35 @@
     return $databaseConnection;
 
   }
+
+
+  $app = new \Slim\App();
+
+  $app->get("/country/{country}", function($request, $response, $args) {
+
+    try {
+
+      $db = getDB();
+
+      $statement = $db->prepare("SELECT * FROM countries WHERE name = :country");
+
+      $statement->bindParam(":country", $args["country"], PDO::PARAM_STR);
+
+      $statement->execute();
+
+      $newResponse = $response->withHeader("Content-type", "application/json");
+      $newResponse = $newResponse->withJSON($statement->fetch(PDO::FETCH_OBJ));
+
+      return $newResponse;
+
+    } catch (PDOException $e) {
+
+      $newResponse = $response->withStatus(500);
+      return $newResponse;
+
+    }
+
+  });
+
+
+  $app->run();
