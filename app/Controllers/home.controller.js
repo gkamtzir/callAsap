3648dc88849bf663a7e063310253ed425a78c39a
George Kamtziridis <georgekam96@gmail.com>
2016-10-13 19:05:03 +0300

    Updated home controller and view

--- app/Controllers/home.controller.js
+++ app/Controllers/home.controller.js
@@ -3,13 +3,51 @@
   angular.module("callAsap")
     .controller("HomeController", HomeController);
 
-  HomeController.$inject = ["$scope", "$state"];
+  HomeController.$inject = ["$scope", "$state", "homeFactory", "searchFactory"];
 
-  function HomeController($scope, $state){
+  function HomeController($scope, $state, homeFactory, searchFactory){
 
     $scope.state = "Home Page";
 
+    $scope.ip = "";
+    $scope.country = "";
+
+    $scope.error = "";
     $scope.$state = $state;
+
+    homeFactory.getCountry($scope.ip)
+      .then(function(response) {
+
+        $scope.response = response.data;
+
+        if($scope.response.status == "fail") {
+
+          $scope.error = "Sorry. We could figure out the country you are currently at.";
+
+        } else {
+
+          searchFactory.getCountry($scope.response.country)
+            .then(function(response) {
+
+              $scope.country = response.data;
+
+            },
+              function(response){
+
+                $scope.error = response.data;
+
+              }
+          );
+
+        }
+
+      },
+        function(response) {
+
+          $scope.error = response.data;
+
+      }
+    );
 
   }
 
--- view/home.php
+++ view/home.php
@@ -5,3 +5,15 @@
 <input type="hidden" ng-init="ip='<?php echo $_SERVER['REMOTE_ADDR']; ?>'">
 
 <h3>Your IP: {{ip}}</h3>
+
+<div ng-show="error == false">
+
+  {{ country }}
+
+</div>
+
+<div ng-show="error">
+
+  <h1>Error.</h1>
+
+</div>
