b81c456a8ecb3b0e00564236d707d19bd19a9447
George Kamtziridis <georgekam96@gmail.com>
2016-12-06 16:40:27 +0200

    OrdedBy: 'name'

--- view/search.php
+++ view/search.php
@@ -15,7 +15,7 @@
         <label for="country">Country:</label>
       </div>
       <div class="col-md-6">
-        <select ng-options="option.Name for option in countries track by option.ID" name="country" id="country" ng-model="country" ng-required="true" class="form-control">
+        <select ng-options="option.Name for option in countries | orderBy: 'Name' track by option.ID" name="country" id="country" ng-model="country" ng-required="true" class="form-control">
           <option value="" disabled selected>Choose country</option>
         </select>
       </div>
