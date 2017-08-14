14719b4a337b8aa01e138da8753ebb67edd0d7c2
George Kamtziridis <georgekam96@gmail.com>
2017-08-14 14:05:59 +0300

    Escaped characters

--- app/Directives/loading.directive.js
+++ app/Directives/loading.directive.js
@@ -9,7 +9,7 @@
 
       return {
           restrict: 'E',
-          template: '<h3><img src='http://www.nasa.gov/multimedia/videogallery/ajax-loader.gif' width='20' height='20' />LOADING...</h3>',
+          template: '<h3><img src=\'http://www.nasa.gov/multimedia/videogallery/ajax-loader.gif\' width=\'20\' height=\'20\' />LOADING...</h3>',
           link: function (scope, elm, attrs)
           {
               scope.isLoading = function () {
